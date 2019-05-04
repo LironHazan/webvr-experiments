9bfeb5d6854350ae12f4d4a044ed8bbb92992a85
liron.hazan <liron.hazan@cybereason.com>
2019-05-04 22:32:01 +0300

    :sparkle: vanilla is sooo muchh fun!!

--- dist/index.js
+++ dist/index.js
@@ -14,6 +14,16 @@ MyAframeApp.soundEntity = document.querySelector('#sound');
 MyAframeApp.textScoreEntity = document.querySelector('#score');
 
 MyAframeApp.store = {clicks: 0, sound: null};
+MyAframeApp.handler = {
+    set (target, key, value) {
+        if (key in target) {
+            target[key] = value;
+            MyAframeApp.onChange();
+        }
+        return target;
+    }
+};
+MyAframeApp.observeStore$ = new Proxy(MyAframeApp.store, MyAframeApp.handler);
 
 MyAframeApp.playSound = async (soundEntity) => {
     soundEntity.setAttribute('src', MyAframeApp.store.sound);
@@ -25,21 +35,10 @@ MyAframeApp.updateText = (entity, text) => {
 };
 
 MyAframeApp.onChange = async () => {
-    MyAframeApp.updateText(MyAframeApp.textScoreEntity, `value: Your Score: ${MyAframeApp.store.clicks}`);
+    MyAframeApp.updateText(MyAframeApp.textScoreEntity,
+        `value: Your Score: ${MyAframeApp.store.clicks}`);
     return MyAframeApp.playSound(MyAframeApp.soundEntity);
 };
-
-MyAframeApp.handler = {
-    set (target, key, value) {
-        if (key in target) {
-            target[key] = value;
-             MyAframeApp.onChange();
-        }
-        return target;
-    }
-};
-
-MyAframeApp.observeStore$ = new Proxy(MyAframeApp.store, MyAframeApp.handler);
 
 
 MyAframeApp();
