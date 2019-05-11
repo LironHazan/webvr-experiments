fa558795c8900cf88b6a7e89554cbfde49f25307
liron.hazan <liron.hazan@cybereason.com>
2019-05-11 23:07:43 +0300

    :sparkle: making a playable game

--- dist/components/instrument.component.js
+++ dist/components/instrument.component.js
@@ -1,30 +1,17 @@
 AFRAME.registerComponent('instrument', {
-    schema: {},
+    schema: {
+    },
     init: function () {
-        const soundEntity = document.querySelector('#sound');
+        this.el.addEventListener('mousedown', () => {
 
-        switch (this.el.id) {
-            case 'guitar':  {
-                this.el.addEventListener('mousedown', () => {
-                    this.el.removeAttribute('particle-system');
-                    this.el.setAttribute('particle-system', 'color: #EFAF00,#000000; maxAge: 2; duration: 0.5; particleCount: 200');
-                    soundEntity.setAttribute('src', './assets/guitar/disco-guitar-notes-2.wav');
-                    soundEntity.play();
-                })
-            } break;
-            case 'drums':  {
-                this.el.addEventListener('mousedown', () => {
-                    soundEntity.setAttribute('src', './assets/drums/178_on-a-roll-drums-A.wav');
-                    soundEntity.play();
-                })
-            } break;
-            case 'keyboard':  {
-                this.el.addEventListener('mousedown', () => {
-                    soundEntity.setAttribute('src', './assets/keyboard/noisy-akai-organ-chord.wav');
-                    soundEntity.play();
-                })
-            }
-        }
+          //  this.el.removeAttribute('particle-system');
+          //  this.el.setAttribute('particle-system', 'color: #EFAF00,#000000; maxAge: 2; duration: 0.5; particleCount: 200');
+        });
+
+        this.el.addEventListener('animationcomplete', () => {
+            this.el.setAttribute('scale', '1 1 1');
+            this.el.components['random-position'].update();
+        })
     }
 });
 
